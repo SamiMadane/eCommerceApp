cb7a3dbcb1810f2b6609466ba1010d874251deeb
SamiMadane <samimadane23@gmail.com>
2023-05-27 12:07:17 +0300

    init my const variable in my app The last thing reached sharedPref

---
+++ lib/config/constant.dart
@@ -0,0 +1,17 @@
+class Constants{
+  static const double deviceWidth = 428;
+  static const double deviceHeight = 926;
+  static const int splachDuration = 3;
+  static const double elevationAppBar = 0;
+  static const double elevationButton = 0;
+  static const int outBoardingTimeDuration = 1;
+  static const int countOfDots = 3;
+  static const double spacingBetweenDots = 15.0;
+  static const double radiusOfDots = 50.0;
+  static const double radiusdotWidth = 12.0;
+  static const double radiusdotHeight = 12.0;
+}
+
+class ConstantsPrefKey {
+  static const String out_boarding_viewed = 'out_boarding_view';
+}
