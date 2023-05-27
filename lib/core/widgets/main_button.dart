6edc7a3c30c9cea9b14846e821dea6ded4edf665
SamiMadane <samimadane23@gmail.com>
2023-05-27 12:10:56 +0300

    widget mainButton The last thing reached sharedPref

---
+++ lib/core/widgets/main_button.dart
@@ -0,0 +1,31 @@
+import 'package:e_commerce/core/extensions/extensions.dart';
+import 'package:flutter/material.dart';
+import '../../config/constant.dart';
+import '../resources/manager_colors.dart';
+import '../resources/manager_sizes.dart';
+
+
+Widget mainButton({
+  required Widget child,
+  void Function() ? onPressed,
+  ShapeBorder? shapeBorder,
+  Color? color,
+  double? minWidth,
+  double? height,
+  double? elevation,
+}) {
+  return MaterialButton(
+    onPressed: onPressed.onNull(),
+    shape: shapeBorder ??
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            ManagerRadius.r12,
+          ),
+        ),
+    color: color.onNull(),
+    minWidth: minWidth.onNull(),
+    height: height.onNull(),
+    elevation: elevation.onNull(),
+    child: child,
+  );
+}
